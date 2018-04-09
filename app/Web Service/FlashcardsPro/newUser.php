caee77e05c6d23acce0d362c6fa7b262ab5ce3e4
ryanfilkins97 <rmfvg5@mail.missouri.edu>
2018-04-09 13:22:19 -0500

    Updated Web Service code to include user creation and login

---
+++ app/Web Service/FlashcardsPro/newUser.php
@@ -0,0 +1,40 @@
+<?php
+$response = array("status" => "success");
+
+$newUsername = $_POST['username'];
+if(!($newUsername)){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+$newPassword = $_POST['password'];
+if(!($newPassword)){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+$hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+
+require('db_credentials.php');
+
+$mysqli = new mysqli($servername, $username, $password, $dbname);
+if($mysqli->connect_error){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+$stmt = $mysqli->prepare("INSERT INTO users(username, password) VALUES(?, ?)");
+
+if(!($stmt->bind_param("ss", $newUsername, $hashedPassword))){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+if(!($stmt->execute())){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+$response['status'] = 'succeeded';
+print(json_encode($response));
+?>
