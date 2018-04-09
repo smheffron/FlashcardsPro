1cca53b87ebe3a53525d896cc221e22a68c9c627
smheffron <smhgzd@mail.missouri.edu>
2018-04-09 00:26:11 -0500

    Created login / create account flow with databinding button handlers

--- app/src/main/java/com/merlinsbeard/flashcardspro/CreateAccountActivity.java
+++ app/src/main/java/com/merlinsbeard/flashcardspro/CreateAccountActivity.java
@@ -1,13 +1,20 @@
 package com.merlinsbeard.flashcardspro;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.merlinsbeard.flashcardspro.databinding.ActivityCreateAccountBinding;
+
 public class CreateAccountActivity extends AppCompatActivity {
 
+    private ActivityCreateAccountBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_account);
+        binding.setActivity(this);
+
+
     }
 }
--- app/src/main/java/com/merlinsbeard/flashcardspro/LoginActivity.java
+++ app/src/main/java/com/merlinsbeard/flashcardspro/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.merlinsbeard.flashcardspro;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.databinding.DataBindingUtil;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
 import com.merlinsbeard.flashcardspro.databinding.LoginActivityBinding;
 
 public class LoginActivity extends AppCompatActivity {
@@ -14,11 +19,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.login_activity);
+        binding.setActivity(this);
 
 
 
 
 
+    }
+
+    public void onClickCreateAccount(View view){
+        Intent intent = new Intent(this,CreateAccountActivity.class);
+        startActivity(intent);
     }
 
 }
