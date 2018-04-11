3e5a474d4cfe4e30c7247a58ecb7bbb23020a58d
ryanfilkins97 <rmfvg5@mail.missouri.edu>
2018-04-10 22:08:34 -0500

    Added ability for user to remain logged in between runs of the app

--- app/src/main/java/com/merlinsbeard/flashcardspro/SetViewActivity.java
+++ app/src/main/java/com/merlinsbeard/flashcardspro/SetViewActivity.java
@@ -1,28 +1,27 @@
 package com.merlinsbeard.flashcardspro;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class SetViewActivity extends AppCompatActivity {
-    public ArrayList<String> mDataset;
-
+    private User user = new User();
+    private ArrayList<String> mDataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_view);
+
+        final SharedPreferences preferences = getApplicationContext().getSharedPreferences("user", Context.MODE_PRIVATE);
+        user.setUsername(preferences.getString("username", ""));
+        user.setUserId(preferences.getInt("userId", -1));
 
         mDataset = new ArrayList<>();
         mDataset.add("pie");
