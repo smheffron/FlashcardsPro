eff5c2698ede810a82bc718dd131d39f5f4eb37a
ryanfilkins97 <rmfvg5@mail.missouri.edu>
2018-04-12 18:56:14 -0500

    Added web service to update card set name

---
+++ app/Web Service/FlashcardsPro/updateSetName.php
@@ -0,0 +1,42 @@
+<?php
+if($data = json_decode(file_get_contents("php://input"), true)){
+    $_POST = $data;
+}
+
+$response = array("status" => "success");
+
+$setId = $_GET['id'] ? $_GET['id'] : -1;
+if($setId == -1){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+$newSetTitle = $_POST['title'] ? $_POST['title'] : '';
+if(!$newSetTitle){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+require('db_credentials.php');
+
+$mysqli = new mysqli($servername, $username, $password, $dbname, $port);
+if($mysqli->connect_error){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+$stmt = $mysqli->prepare("UPDATE sets SET name = ? WHERE id = ?");
+
+if(!($stmt->bind_param("si", $newSetTitle, $setId))){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+if(!($stmt->execute())){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+$response['status'] = 'succeeded';
+print(json_encode($response));
+?>
