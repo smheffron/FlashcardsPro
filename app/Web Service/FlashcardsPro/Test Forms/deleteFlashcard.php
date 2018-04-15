ccee4d12c765f40b672cc09391db2f04ccc4bed6
smheffron <smhgzd@mail.missouri.edu>
2018-04-15 12:40:41 -0500

    Delete scripts / allowing deletion of notecards from db

---
+++ app/Web Service/FlashcardsPro/Test Forms/deleteFlashcard.php
@@ -0,0 +1,32 @@
+<?php
+$response = array("status" => "success");
+
+$cardId = $_GET['cardId'] ? $_GET['cardId'] : -1;
+if($cardId == -1){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+require('db_credentials.php');
+
+$mysqli = new mysqli($servername, $username, $password, $dbname, $port);
+if($mysqli->connect_error){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+$stmt = $mysqli->prepare("DELETE FROM cards WHERE cardId = ?");
+
+if(!($stmt->bind_param("i", $cardId))){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+if(!($stmt->execute())){
+    $response['status'] = 'failed';
+    exit(json_encode($response));
+}
+
+$response['status'] = 'succeeded';
+print(json_encode($response));
+?>
--- app/src/main/java/com/merlinsbeard/flashcardspro/FlashCardView.java
+++ app/src/main/java/com/merlinsbeard/flashcardspro/FlashCardView.java
@@ -95,6 +95,44 @@ public class FlashCardView extends AppCompatActivity {
 
     public void handleDeleteClick(final int i) {
 
+        int idToRemove = mDataset.get(i).getFlashcardId();
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://ec2-18-188-60-72.us-east-2.compute.amazonaws.com/FlashcardsPro/deleteFlashcard.php?cardId=" + idToRemove;
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if(response.getString("status").equals("succeeded")){
+                        mDataset.remove(mDataset.get(i));
+
+                        recyclerView.removeViewAt(i);
+                        recyclerAdapter.notifyItemRemoved(i);
+                        recyclerAdapter.notifyItemRangeChanged(i, mDataset.size());
+
+                        Log.d("The values left are: ", mDataset.toString());
+
+                        if(mDataset.isEmpty()){
+                            emptyView.setVisibility(View.VISIBLE);
+                        }
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "Could not connect to server", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("LOGIN ERROR", error.toString());
+            }
+        });
+
+        queue.add(request);
+
     }
 
     public void handleRenameClick(final int i ) {
