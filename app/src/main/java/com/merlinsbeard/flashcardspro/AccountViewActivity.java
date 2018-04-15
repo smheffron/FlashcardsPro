f1097eaa5bed2bececf3a31885519032e0c0489b
ryanfilkins97 <rmfvg5@mail.missouri.edu>
2018-04-15 12:18:28 -0500

    Added ability to change username

--- app/src/main/java/com/merlinsbeard/flashcardspro/AccountViewActivity.java
+++ app/src/main/java/com/merlinsbeard/flashcardspro/AccountViewActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -48,7 +47,65 @@ public class AccountViewActivity extends Activity {
     }
 
     public void onClickChangeUsername(View view){
+        final Activity thisActivity = this;
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String url = "http://ec2-18-188-60-72.us-east-2.compute.amazonaws.com/FlashcardsPro/updateUsername.php?id=" + user.getUserId();
 
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.change_username_popup);
+        dialog.setTitle("Change username");
+        dialog.show();
+
+        final TextView newUsername = dialog.findViewById(R.id.newUsernameFromChangeUsername);
+        final TextView password = dialog.findViewById(R.id.passwordFromChangeUsername);
+
+        dialog.findViewById(R.id.dialogButtonCancelFromChangeUsername).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.findViewById(R.id.dialogButtonRenameFromChangeUsername).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String,String> params = new HashMap<>();
+                params.put("password", String.valueOf(password.getText()));
+                params.put("newUsername", String.valueOf(newUsername.getText()));
+
+                final SharedPreferences preferences = getApplicationContext().getSharedPreferences("user", Context.MODE_PRIVATE);
+
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params), new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if(response.getString("status").equals("succeeded")){
+                                user.setUsername(String.valueOf(newUsername.getText()));
+                                SharedPreferences.Editor editor = preferences.edit();
+                                editor.putString("username", user.getUsername());
+                                editor.apply();
+                            }
+                            else if(response.has("reason") || response.getString("reason").equals("name taken")){
+                                Toast.makeText(getApplicationContext(), "Username is already taken", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(), "Could not connect to server", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(getApplicationContext(), "Could not connect to server", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Could not connect to server", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                queue.add(request);
+                dialog.dismiss();
+            }
+        });
     }
 
     public void onClickChangePassword(View view){
@@ -58,6 +115,7 @@ public class AccountViewActivity extends Activity {
     public void onClickLogout(View view){
         SharedPreferences.Editor editor = preferences.edit();
         editor.remove("userId");
+        editor.remove("username");
         editor.apply();
         Intent intent = new Intent(this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -108,14 +166,13 @@ public class AccountViewActivity extends Activity {
                                 Toast.makeText(getApplicationContext(), "Could not connect to server", Toast.LENGTH_SHORT).show();
                             }
                         } catch (JSONException e) {
-                            //handle this better
-                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "Could not connect to server", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.d("LOGIN ERROR", error.toString());
+                        Toast.makeText(getApplicationContext(), "Could not connect to server", Toast.LENGTH_SHORT).show();
                     }
                 });
 
