61347d59e4edca70fd782c283eabd7e29e1d34d3
ryanfilkins97 <rmfvg5@mail.missouri.edu>
2018-04-21 17:00:19 -0500

    Code cleanup and documentation

--- app/src/main/java/com/merlinsbeard/flashcardspro/activities/StudyActivity.java
+++ app/src/main/java/com/merlinsbeard/flashcardspro/activities/StudyActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.app.NavUtils;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Slide;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.Gravity;
 import android.view.MenuItem;
@@ -36,8 +35,9 @@ public class StudyActivity extends AppCompatActivity implements GestureDetector.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+
+        // If device supports enter and exit transitions, enable them and set them up
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS);
 
@@ -50,25 +50,21 @@ public class StudyActivity extends AppCompatActivity implements GestureDetector.
 
         setContentView(R.layout.activity_study);
 
+        // Allow up navigation
         if(getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        // Get flashcard data from previous activity
         mDataSet = getIntent().getParcelableArrayListExtra("data");
         positionClicked = getIntent().getIntExtra("position", -1);
 
+        // Set card text to display front text of card that was selected
         cardText = findViewById(R.id.flashcardText);
-        ConstraintLayout constraintLayout = findViewById(R.id.individualCardView);
-
-        setName = String.valueOf(getIntent().getCharSequenceExtra("setName"));
-        setTitle(setName);
-
-        setId = getIntent().getIntExtra("setId", -1);
-
-        Log.d("SET ID:", String.valueOf(setId));
-
         cardText.setText(mDataSet.get(positionClicked).getFrontText());
 
+        // Make card text change from front to back or back to front when the view is long pressed
+        ConstraintLayout constraintLayout = findViewById(R.id.individualCardView);
         constraintLayout.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
@@ -85,9 +81,16 @@ public class StudyActivity extends AppCompatActivity implements GestureDetector.
             }
         });
 
+        // Set title in action bar to the name of the set
+        setName = String.valueOf(getIntent().getCharSequenceExtra("setName"));
+        setTitle(setName);
+
+        setId = getIntent().getIntExtra("setId", -1);
+
         detector = new GestureDetectorCompat(this,this);
     }
 
+    // Using custom gesture detection in onFling(), prevent the default implementations of these from interfering
     @Override
     public boolean onDown(MotionEvent motionEvent) {
         return false;
@@ -95,7 +98,7 @@ public class StudyActivity extends AppCompatActivity implements GestureDetector.
 
     @Override
     public void onShowPress(MotionEvent motionEvent) {
-
+        // Do nothing
     }
 
     @Override
@@ -110,33 +113,7 @@ public class StudyActivity extends AppCompatActivity implements GestureDetector.
 
     @Override
     public void onLongPress(MotionEvent motionEvent) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-
-        if(motionEvent.getX() - motionEvent1.getX()<0 && Math.abs(motionEvent.getX() - motionEvent1.getX()) > 150){
-            if(positionClicked - 1 >= 0){
-                cardText.setText(mDataSet.get(--positionClicked).getFrontText());
-                isFront=true;
-            }
-        }
-
-
-        if(motionEvent.getX() - motionEvent1.getX()>0 && Math.abs(motionEvent.getX() - motionEvent1.getX()) > 150){
-            //right swipe
-            if(positionClicked + 1 < mDataSet.size()){
-                cardText.setText(mDataSet.get(++positionClicked).getFrontText());
-                isFront=true;
-            }
-        }
-
-        if(motionEvent.getY() - motionEvent1.getY() < 0 && Math.abs(motionEvent.getY() - motionEvent1.getY()) > 150 && Math.abs(motionEvent.getX() - motionEvent1.getX()) < 150){
-            navigateUpToFlashcardActivity();
-        }
-
-        return true;
+        // Do nothing
     }
 
     @Override
@@ -149,6 +126,34 @@ public class StudyActivity extends AppCompatActivity implements GestureDetector.
     public boolean dispatchTouchEvent(MotionEvent ev) {
         detector.onTouchEvent(ev);
         return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+
+        // Left to right swipe, move to previous card in data set if it exists, display front text
+        if(motionEvent.getX() - motionEvent1.getX()<0 && Math.abs(motionEvent.getX() - motionEvent1.getX()) > 150){
+            if(positionClicked - 1 >= 0){
+                cardText.setText(mDataSet.get(--positionClicked).getFrontText());
+                isFront=true;
+            }
+        }
+
+        // Right to left swipe, move to next card in data set if it exists, display front text
+        if(motionEvent.getX() - motionEvent1.getX()>0 && Math.abs(motionEvent.getX() - motionEvent1.getX()) > 150){
+            //right swipe
+            if(positionClicked + 1 < mDataSet.size()){
+                cardText.setText(mDataSet.get(++positionClicked).getFrontText());
+                isFront=true;
+            }
+        }
+
+        // Down swipe, navigate back to FlashcardActivity
+        if(motionEvent.getY() - motionEvent1.getY() < 0 && Math.abs(motionEvent.getY() - motionEvent1.getY()) > 150 && Math.abs(motionEvent.getX() - motionEvent1.getX()) < 150){
+            navigateUpToFlashcardActivity();
+        }
+
+        return true;
     }
 
     @Override
@@ -168,6 +173,7 @@ public class StudyActivity extends AppCompatActivity implements GestureDetector.
             intent.putExtra("setId", setId);
             intent.putExtra("setName", setName);
 
+            // Navigate back to FlashcardActivity, use transitions if supported
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
             }
