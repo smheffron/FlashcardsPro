c556858ca0d86ed8f3fac66e4a435510b946c7ae
Josh Westbrook <jwgy5@mail.missouri.edu>
2018-04-30 23:44:40 -0500

    added href to sets list

--- website_app/global.js
+++ website_app/global.js
@@ -55,7 +55,7 @@ function initSetsList() {
             if(data.status === 'succeeded') {
                 $('#setsList').append('<ul></ul>');
                 $.each(data.sets, function(index, set) {
-                    $('#setsList ul').append('<li value="' + set.setId + '">' + set.setName + '</li>');
+                    $('#setsList ul').append('<li href="?set=' + set.setId + '">' + set.setName + '</li>');
                 });
             }
         }
