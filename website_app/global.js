802988b28ef4735891a9b32cfda229fa0e1e66a3
Josh Westbrook <jwgy5@mail.missouri.edu>
2018-04-30 22:44:40 -0500

    added user feedback to login form on index.html

--- website_app/global.js
+++ website_app/global.js
@@ -1,5 +1,45 @@
 function verifyLogin() {
-    if(Cookies.get('logged_in') == null && window.location.pathname != '/index.html') {
+    if((Cookies.get('logged_in') == null || Cookies.get('logged_in') == undefined) && window.location.pathname != '/index.html') {
         window.location = '/index.html';
     }
+}
+
+function initLoginPage() {
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'http://ec2-18-188-60-72.us-east-2.compute.amazonaws.com/FlashcardsPro/verifyLogin.php',
+            type: 'post',
+            dataType: 'json',
+            data: {
+                    'username': $('#username').val(),
+                    'password': $('#password').val()
+                  },
+            success: function(data) {
+                if(data.userId != null && data.status === 'succeeded' && data.login === 'succeeded') {
+                    Cookies.set('logged_in', data.userId);
+                    window.location = 'home.html';
+                }else if(data.status === 'succeeded' && data.login === 'failed') {
+                    if($('#errorMessage').length > 0) {
+                        $('#errorMessage').text('Incorrect credentials. Please try again.')
+                    }else {
+                        $('.container').append('<p id="errorMessage">Incorrect credentials. Please try again.</p>');
+                    }
+                }else {
+                    if($('#errorMessage').length > 0) {
+                        $('#errorMessage').text('There was an error processing your request, please contact the system administrator.')
+                    }else {
+                        $('.container').append('<p id="errorMessage">There was an error processing your request, please contact the system administrator.</p>');
+                    }
+                }
+            },
+            error: function(data) {
+                if($('#errorMessage').length > 0) {
+                    $('#errorMessage').text('There was an error processing your request, please contact the system administrator.')
+                }else {
+                    $('.container').append('<p id="errorMessage">There was an error processing your request, please contact the system administrator.</p>');
+                }
+            }
+        });
+    });
 }
