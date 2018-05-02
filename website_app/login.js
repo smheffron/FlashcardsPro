c09866fe53a2703795020b13b25cb94ce22b5039
Josh Westbrook <jwgy5@mail.missouri.edu>
2018-05-02 15:11:50 -0500

    fixed create account errors (last time)

--- website_app/login.js
+++ website_app/login.js
@@ -40,43 +40,55 @@ function initLoginPage() {
     
     $('#createAccountForm form').submit(function(e) {
         e.preventDefault();
-        $.ajax({
-            url: 'http://ec2-18-188-60-72.us-east-2.compute.amazonaws.com/FlashcardsPro/newUser.php',
-            type: 'post',
-            dataType: 'json',
-            data: {
-                    'username': $('#newUserUsername').val(),
-                    'password': $('#newUserPassword').val()
-                  },
-            success: function(data) {
-                if(data.userId != null && data.status === 'succeeded') {
-                    Cookies.set('logged_in', data.userId);
-                    Cookies.set('username', $('#newUserUsername').val());
-                    window.location = '/home.html';
-                }else if(data.reason === 'name taken') {
-                    if($('.errorMessage').length > 0) {
-                        $('.errorMessage').text('Username taken. Please try a different one.')
+        if($('#newUserPassword').val() === $('#newUserConfirmPassword').val()) {
+            $.ajax({
+                url: 'http://ec2-18-188-60-72.us-east-2.compute.amazonaws.com/FlashcardsPro/newUser.php',
+                type: 'post',
+                dataType: 'json',
+                data: {
+                        'username': $('#newUserUsername').val(),
+                        'password': $('#newUserPassword').val()
+                      },
+                success: function(data) {
+                    if(data.userId != null && data.status === 'succeeded') {
+                        Cookies.set('logged_in', data.userId);
+                        Cookies.set('username', $('#newUserUsername').val());
+                        window.location = '/home.html';
+                    }else if(data.reason === 'name taken') {
+                        if($('.errorMessage').length > 0) {
+                            $('.errorMessage').text('Username taken. Please try a different one.')
+                        }else {
+                            $('#createAccountForm').before('<p class="errorMessage">Username taken. Please try a different one.</p>');
+                        }
+                        $('#newUserUsername, #newUserPassword, #newUserConfirmPassword').val('');
                     }else {
-                        $('#createAccountForm').before('<p class="errorMessage">Username taken. Please try a different one.</p>');
+                        console.dir(data);
+                        if($('.errorMessage').length > 0) {
+                            $('.errorMessage').text('There was an error processing your request, please contact the system administrator.')
+                        }else {
+                            $('#createAccountForm').before('<p class="errorMessage">There was an error processing your request, please contact the system administrator.</p>');
+                        }
+                        $('#newUserPassword, #newUserConfirmPassword').val('');
                     }
-                }else {
+                },
+                error: function(data) {
                     console.dir(data);
                     if($('.errorMessage').length > 0) {
                         $('.errorMessage').text('There was an error processing your request, please contact the system administrator.')
                     }else {
                         $('#createAccountForm').before('<p class="errorMessage">There was an error processing your request, please contact the system administrator.</p>');
                     }
+                    $('#newUserPassword, #newUserConfirmPassword').val('');
                 }
-            },
-            error: function(data) {
-                console.dir(data);
-                if($('.errorMessage').length > 0) {
-                    $('.errorMessage').text('There was an error processing your request, please contact the system administrator.')
-                }else {
-                    $('#createAccountForm').before('<p class="errorMessage">There was an error processing your request, please contact the system administrator.</p>');
-                }
+            });
+        }else {
+            if($('.errorMessage').length > 0) {
+                $('.errorMessage').text('The passwords did not match.')
+            }else {
+                $('#createAccountForm').before('<p class="errorMessage">The passwords did not match.</p>');
             }
-        });
+            $('#newUserPassword, #newUserConfirmPassword').val('');
+        }
     });
 }
 
