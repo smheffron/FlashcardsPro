d9eea8f053366196ef7e6e26c0e21c03b54726b6
Josh Westbrook <jwgy5@mail.missouri.edu>
2018-05-01 20:41:41 -0500

    Added functionality to Account page

---
+++ website_app/account.js
@@ -0,0 +1,121 @@
+function initEditUsername() {
+    var id = Cookies.get('logged_in');
+    var formSkeleton = '<label for="newUsername">New Username: </label><br>'
+                     + '<input type="text" id="newUsername" placeholder="New Username..." /><br>'
+                     + '<input type="password" id="password" placeholder="Password..." /><br>'
+                     + '<button class="btn btn-primary onclick="editUsername(' + id + ')">Update</button>';
+    $('#account').html(formSkeleton);
+    $('#editPasswordBtn, #editUsernameBtn, #deleteUserBtn').addClass('disabled');
+}
+
+function editUsername(id) {
+    var newUsername = $('#newUsername').val();
+    var password = $('#password').val();
+    
+    $.ajax({
+        url: 'http://ec2-18-188-60-72.us-east-2.compute.amazonaws.com/FlashcardsPro/updateUsername.php?id=' + id,
+        type: 'post',
+        dataType: 'json',
+        data: {'newUsername': newUsername, 'password': password},
+        success: function(data) {
+            console.dir(data);
+            if(data.status === 'succeeded') {
+                $('#userDataWrapper').before('<p class="text-success">Successfully changed username to "' + newUsername + '"!</p>');
+                $('#account').empty();
+                $('#editPasswordBtn, #editUsernameBtn, #deleteUserBtn').removeClass('disabled');
+            }else if(data.reason === 'authentication failure'){
+                $('#userDataWrapper').before('<p class="text-danger">Incorrect password provided!</p>');
+            }else {
+                $('#account').prepend('<p class="text-danger">There was an error updating your username.</p>');
+            }
+        },
+        error: function(data) {
+            console.dir(data);
+            $('#account').prepend('<p class="text-danger">There was an error updating your username.</p>');
+        }
+    });
+}
+
+function initEditPassword() {
+    var id = Cookies.get('logged_in');
+    var formSkeleton = '<label for="currentPassword">Current Password: </label><br>'
+                     + '<input type="password" id="currentPassword" /><br>'
+                     + '<label for="newPassword">New Password: </label><br>'
+                     + '<input type="password" id="newPassword" /><br>'
+                     + '<label for="repeatNewPassword">Confirm New Password: </label><br>'
+                     + '<input type="password" id="confirmNewPassword" /><br>'
+                     + '<button class="btn btn-primary onclick="editPassword(' + id + ')">Update</button>';
+    $('#account').html(formSkeleton);
+    $('#editPasswordBtn, #editUsernameBtn, #deleteUserBtn').addClass('disabled');
+}
+
+function editPassword(id) {
+    var currentPassword = $('#currentPassword').val();
+    var newPassword = $('#newPassword').val();
+    var confirmNewPassword = $('#confirmNewPassword').val();
+    
+    if(newPassword === confirmNewPassword) {
+        $.ajax({
+            url: 'http://ec2-18-188-60-72.us-east-2.compute.amazonaws.com/FlashcardsPro/updatePassword.php?id=' + id,
+            type: 'post',
+            dataType: 'json',
+            data: {'oldPassword': currentPassword, 'newPassword': newPassword},
+            success: function(data) {
+                console.dir(data);
+                if(data.status === 'succeeded') {
+                    $('#userDataWrapper').before('<p class="text-success">Successfully changed password!</p>');
+                    $('#account').empty();
+                    $('#editPasswordBtn, #editUsernameBtn, #deleteUserBtn').removeClass('disabled');
+                }else if(data.reason === 'authentication failure'){
+                    $('#account').prepend('<p class="text-danger">Incorrect password provided!</p>');
+                    $('#currentPassword, #newPassword, #confirmNewPassword').val('');
+                }else {
+                    $('#account').prepend('<p class="text-danger">There was an error updating your password.</p>');
+                    $('#currentPassword, #newPassword, #confirmNewPassword').val('');
+                }
+            },
+            error: function(data) {
+                console.dir(data);
+                $('#account').prepend('<p class="text-danger">There was an error updating your password.</p>');
+                $('#currentPassword, #newPassword, #confirmNewPassword').val('');
+            }
+        });
+    }else {
+        $('#account').prepend('<p class="text-danger">The passwords did not match.</p>');
+        $('#currentPassword, #newPassword, #confirmNewPassword').val('');
+    }
+}
+
+function initDeleteUser() {
+    var id = Cookies.get('logged_in');
+    var formSkeleton = '<p>Please enter your password to confirm the deletion of your account. <b>THIS CANNOT BE UNDONE!</b></p><br>'
+                     + '<label for="password">Current Password: </label><br>''
+                     + '<input type="password" id="password" placeholder="Password..." /><br>'
+                     + '<button class="btn btn-danger onclick="deleteUser(' + id + ')">Permanently Delete</button>';
+    $('#account').html(formSkeleton);
+}
+
+function deleteUser(id) {
+    var password = $('#password').val();
+    
+    $.ajax({
+        url: 'http://ec2-18-188-60-72.us-east-2.compute.amazonaws.com/FlashcardsPro/deleteUser.php?id=' + id,
+        type: 'post',
+        dataType: 'json',
+        data: {'password': password},
+        success: function(data) {
+            console.dir(data);
+            if(data.status === 'succeeded') {
+                logout();
+            }else if(data.reason === 'authentication failure'){
+                $('#account').prepend('<p class="text-danger">Incorrect password provided!</p>');
+            }else {
+                $('#account').prepend('<p class="text-danger">There was an error deleting your user.</p>');
+            }
+        },
+        error: function(data) {
+            console.dir(data);
+            $('#account').prepend('<p class="text-danger">There was an error deleting your user.</p>');
+        }
+    });
+}
---
+++ website_app/login.js
@@ -0,0 +1,39 @@
+function initLoginPage() {
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'http://ec2-18-188-60-72.us-east-2.compute.amazonaws.com/FlashcardsPro/verifyLogin.php',
+            type: 'post',
+            dataType: 'json',
+            data: {
+                    'username': $('#username').val(),
+                    'password': $('#password').val()
+                  },
+            success: function(data) {
+                if(data.userId != null && data.status === 'succeeded' && data.login === 'succeeded') {
+                    Cookies.set('logged_in', data.userId);
+                    window.location = 'home.html';
+                }else if(data.status === 'succeeded' && data.login === 'failed') {
+                    if($('.errorMessage').length > 0) {
+                        $('.errorMessage').text('Incorrect credentials. Please try again.')
+                    }else {
+                        $('form').before('<p class="errorMessage">Incorrect credentials. Please try again.</p>');
+                    }
+                }else {
+                    if($('.errorMessage').length > 0) {
+                        $('.errorMessage').text('There was an error processing your request, please contact the system administrator.')
+                    }else {
+                        $('form').before('<p class="errorMessage">There was an error processing your request, please contact the system administrator.</p>');
+                    }
+                }
+            },
+            error: function(data) {
+                if($('.errorMessage').length > 0) {
+                    $('.errorMessage').text('There was an error processing your request, please contact the system administrator.')
+                }else {
+                    $('form').before('<p class="errorMessage">There was an error processing your request, please contact the system administrator.</p>');
+                }
+            }
+        });
+    });
+}
